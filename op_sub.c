dc57d23e6a18abb336db4bedd1fe247f84c665ee
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-24 01:48:09 -0700

    handled the sub opcode

---
+++ op_sub.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * sub - Subtracts the top element of the stack from the second top element.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number being processed.
+ *
+ * Return: None.
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n -= (*stack)->n;
+	pop(stack, line_number);
+}
