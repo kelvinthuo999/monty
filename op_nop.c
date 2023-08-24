0c698e2088156e68c6809f66f243f56761032003
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-24 01:44:16 -0700

    added the nop opcode

---
+++ op_nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * nop - Doesn't do anything.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number being processed.
+ *
+ * Return: None.
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+	/* Do nothing */
+}
