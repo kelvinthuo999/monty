e2a10f2ced2217beb14905857e3dc3ea30030a07
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-24 01:26:53 -0700

    added the swap op_code

---
+++ op_swap.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * swap - Swaps the top two elements of the stack.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number being processed.
+ *
+ * Return: None.
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	/* variable declaration */
+	int temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+}
