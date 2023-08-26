75148f89efb7e2c603b56b779e9900cf1c738f83
root <calebnzioka2027@gmail.com>
2023-08-26 12:18:26 -0700

    Header changes

--- monty.h
+++ monty.h
@@ -8,8 +8,8 @@
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
+ * @prev: points to the previous element of the data structure
+ * @next: points to the next element of the data structure
  *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
